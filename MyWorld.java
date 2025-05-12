c7d81804cb0b3e91cde7c514689fbc4d0366cb3c
Dennisonung <Dennisonung@gmail.com>
2025-05-12 09:16:25 -0400

    added label and SimpleTimer, my screen

--- MyWorld.java
+++ MyWorld.java
@@ -1,13 +1,20 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    private int score = 0;
     
+    private Label scoreLabel;
     Elephant elephant;
     
     public MyWorld() {
         super(900, 600, 1);
         elephant = new Elephant();
         addObject(elephant, getWidth()/2, getHeight()/2);
+        
+        scoreLabel = new Label("Score: 0",30);
+        
+        addObject(scoreLabel,scoreLabel.getFontSize()/2 + 30, scoreLabel.getFontSize()/2);
+
     }
     
     public void createApple()
