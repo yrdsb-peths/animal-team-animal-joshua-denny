e725632e9aff0a9f8d11f88b2f277db1a5966dbb
Dennisonung <Dennisonung@gmail.com>
2025-05-12 09:14:58 -0400

    Base world

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,19 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    
+    Elephant elephant;
+    
     public MyWorld() {
-        super(600, 400, 1);
+        super(900, 600, 1);
+        elephant = new Elephant();
+        addObject(elephant, getWidth()/2, getHeight()/2);
+    }
+    
+    public void createApple()
+    {
+        //Apple apple = new Apple();
+        //apple.setSpeed(level);
+        //addObject(apple, Greenfoot.getRandomNumber(getWidth()), 0);
     }
 }
