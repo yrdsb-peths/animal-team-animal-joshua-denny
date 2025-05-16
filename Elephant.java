bebe3efbc5a2d36b3aaa5693e714922554b69a4c
herrdorrsnorre <77855285+herrdorrsnorre@users.noreply.github.com>
2025-05-15 22:56:23 -0400

    Fixed jumping bugs and added directional jumping

--- Elephant.java
+++ Elephant.java
@@ -10,6 +10,8 @@ public class Elephant extends Actor
     boolean jumping = false;
     boolean justLanded = false;
     int landingFrameCounter = 0;
+    int landingFrameDelay = 5;  // frames to wait per landing animation frame
+    int landingFrameTimer = 0;
 
     // Audio
     GreenfootSound elephantSound = new GreenfootSound("elephant-scream.mp3");
@@ -19,7 +21,9 @@ public class Elephant extends Actor
     GreenfootImage idleLeft[]   = new GreenfootImage[8];
     GreenfootImage runRight[]   = new GreenfootImage[8];
     GreenfootImage runLeft[]    = new GreenfootImage[8];
-    GreenfootImage jumpFrames[] = new GreenfootImage[10];  // jump0.png to jump9.png
+    GreenfootImage jumpRight[]  = new GreenfootImage[10];  // jump0.png to jump9.png facing right
+    GreenfootImage jumpLeft[]   = new GreenfootImage[10];  // mirrored jump images for left
+    final int floorHeight = 50; // adjust this to your floor/platform height
 
     String facing = "right";
     SimpleTimer animationTimer = new SimpleTimer();
@@ -27,36 +31,39 @@ public class Elephant extends Actor
 
     public Elephant() 
     {
+        setRotation(0);
+
+        // Load idle right and left
         for (int i = 0; i < idleRight.length; i++) 
         {
             idleRight[i] = new GreenfootImage("images/idleAnimation/idle" + i + ".png");
             idleRight[i].scale(100, 100);
-        }
 
-        for (int i = 0; i < idleLeft.length; i++) 
-        {
             idleLeft[i] = new GreenfootImage("images/idleAnimation/idle" + i + ".png");
             idleLeft[i].mirrorHorizontally();
             idleLeft[i].scale(100, 100);
         }
 
+        // Load running right and left
         for (int i = 0; i < runRight.length; i++) 
         {
             runRight[i] = new GreenfootImage("images/running/run" + i + ".png");
             runRight[i].scale(100, 100);
-        }
 
-        for (int i = 0; i < runLeft.length; i++) 
-        {
             runLeft[i] = new GreenfootImage("images/running/run" + i + ".png");
             runLeft[i].mirrorHorizontally();
             runLeft[i].scale(100, 100);
         }
 
-        for (int i = 0; i < jumpFrames.length; i++) 
+        // Load jump frames right and left (mirrored)
+        for (int i = 0; i < jumpRight.length; i++) 
         {
-            jumpFrames[i] = new GreenfootImage("images/jump/jump" + i + ".png");
-            jumpFrames[i].scale(100, 100);
+            jumpRight[i] = new GreenfootImage("images/jump/jump" + i + ".png");
+            jumpRight[i].scale(100, 100);
+
+            jumpLeft[i] = new GreenfootImage("images/jump/jump" + i + ".png");
+            jumpLeft[i].mirrorHorizontally();
+            jumpLeft[i].scale(100, 100);
         }
 
         animationTimer.mark();
@@ -105,36 +112,42 @@ public class Elephant extends Actor
 
     public void handleJumping()
     {
-        yVelocity += gravity;
-        setLocation(getX(), getY() + yVelocity);
-
-        int groundLevel = getWorld().getHeight() - getImage().getHeight() / 2;
+        if (!onGround) 
+        {
+            yVelocity += gravity;
+            setLocation(getX(), getY() + yVelocity);
+        }
 
         if (!onGround)
         {
             justLanded = false;
 
+            // Set jumping animation frames based on yVelocity
+            GreenfootImage[] jumpSet = (facing.equals("right")) ? jumpRight : jumpLeft;
+
             if (yVelocity < -10) {
-                setImage(jumpFrames[1]);
+                setImage(jumpSet[1]);
             } 
             else if (yVelocity < -3) {
-                setImage(jumpFrames[2]);
+                setImage(jumpSet[2]);
             } 
             else if (yVelocity <= 0) {
-                setImage(jumpFrames[3]); // Apex
+                setImage(jumpSet[3]); // Apex
             } 
             else if (yVelocity <= 6) {
-                setImage(jumpFrames[4]); // Descending
+                setImage(jumpSet[4]); // Descending
             } 
             else if (yVelocity <= 12) {
-                setImage(jumpFrames[5]); // Faster descent
+                setImage(jumpSet[5]); // Faster descent
             } 
             else {
-                setImage(jumpFrames[6]); // Near landing
+                setImage(jumpSet[6]); // Near landing
             }
         }
 
         // Detect landing
+        int groundLevel = getWorld().getHeight() - floorHeight - getImage().getHeight() / 2;
+
         if (getY() >= groundLevel)
         {
             if (!onGround)
@@ -145,20 +158,52 @@ public class Elephant extends Actor
                 jumping = false;
                 justLanded = true;
                 landingFrameCounter = 0;
+                landingFrameTimer = 0;
             }
         }
 
-        // Play landing animation only once after landing
+        // Play landing animation only once after landing with slowdown and squash/stretch
         if (justLanded)
         {
+            GreenfootImage[] jumpSet = (facing.equals("right")) ? jumpRight : jumpLeft;
+
             if (landingFrameCounter < 3)
             {
-                setImage(jumpFrames[7 + landingFrameCounter]);
-                landingFrameCounter++;
+                // Slow down animation: update frame every landingFrameDelay acts
+                if (landingFrameTimer == 0) 
+                {
+                    setImage(jumpSet[7 + landingFrameCounter]);
+
+                    // Add squash/stretch on impact frame (landingFrameCounter == 1)
+                    if (landingFrameCounter == 1) 
+                    {
+                        GreenfootImage img = getImage();
+                        img.scale((int)(img.getWidth() * 1.1), (int)(img.getHeight() * 0.9)); // wider & shorter
+                        setImage(img);
+                    }
+                    else
+                    {
+                        // Reset scale on other frames
+                        GreenfootImage img = jumpSet[7 + landingFrameCounter];
+                        img.scale(100, 100);
+                        setImage(img);
+                    }
+                }
+
+                landingFrameTimer++;
+                if (landingFrameTimer >= landingFrameDelay)
+                {
+                    landingFrameTimer = 0;
+                    landingFrameCounter++;
+                }
             }
             else
             {
                 justLanded = false; // Done landing animation
+
+                // Reset to idle image facing correct direction after landing animation
+                GreenfootImage[] idleSet = (facing.equals("right")) ? idleRight : idleLeft;
+                setImage(idleSet[0]);
             }
         }
     }
